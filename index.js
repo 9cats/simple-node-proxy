7d20cadd0d6d96bb21d2827bd87be78652264f8c
9cats <58725483+9cats@users.noreply.github.com>
2023-10-13 19:35:03 +0000

    use stream

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 3000; // 你可以选择其他的端口号
 
 app.use('/', async (req, res) => {
-  const targetUrl = req.originalUrl.slice(1); // 移除前面的斜线 /
+  const targetUrl = req.originalUrl.slice(1);
 
   if (!targetUrl.startsWith('http')) {
     return res.status(400).send('Invalid URL');
@@ -13,15 +13,19 @@ app.use('/', async (req, res) => {
 
   try {
     const response = await fetch(targetUrl);
-    const body = await response.buffer();
 
     // 设置从原始响应中获取的所有头部
     for (const [key, value] of Object.entries(response.headers.raw())) {
       res.setHeader(key, value);
     }
 
-    return res.status(response.status).send(body);
+    // 使用流式传输
+    response.body.pipe(res).on('error', err => {
+      console.error('Stream error:', err);
+      res.status(500).end();
+    });
   } catch (err) {
+    console.error('Fetch error:', err);
     return res.status(500).send('Error occurred');
   }
 });
